c00236c929ac96c0be8af89b8effe353aec1d25a
Johnnie Pittman <jpittman@group6.net>
2025-02-13 00:56:10 -0500

    Update cleanUp.sh
    Updated the cleanup script to remove things correctly.

--- scripts/cleanUp.sh
+++ scripts/cleanUp.sh
@@ -2,4 +2,4 @@
 #
 # A little script to clean up the database files when doing a refresh.
 
-find packs -type f -print | grep -i -e ldb -e log -e current -e lock -e manifest
+find packs -type f -print | grep -i -e ldb -e log -e current -e lock -e manifest | grep -vi scurlock | grep -vi lockpicks | grep -vi G__Manifest | xargs rm
